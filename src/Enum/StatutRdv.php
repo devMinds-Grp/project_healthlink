8da44a0f7dcb57fc9c7b978597bcb3797c8a9eb4
AmeniChakroun <amenichakroun62@gmail.com>
2025-02-17 09:16:59 +0100

    apres final

--- src/Enum/StatutRdv.php
+++ src/Enum/StatutRdv.php
@@ -4,26 +4,16 @@ namespace App\Enum;
 
 enum StatutRdv: string
 {
-<<<<<<< HEAD
     case EN_ATTENTE = 'en_attente';
-=======
->>>>>>> master
     case CONFIRME = 'confirme';
     case ANNULE = 'annule';
 
     public function label(): string
     {
         return match ($this) {
-<<<<<<< HEAD
             self::EN_ATTENTE => 'En attente',
-=======
->>>>>>> master
             self::CONFIRME => 'Confirmé',
             self::ANNULE => 'Annulé',
         };
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> master
--- src/Enum/TypeRdv.php
+++ src/Enum/TypeRdv.php
@@ -1,8 +1,4 @@
 <?php
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 namespace App\Enum;
 
 enum TypeRdv: string
